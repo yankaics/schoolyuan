f0555c0f812b2004f336b5dbc92e916e4714fcb3
sealsun <sunkun201309@163.com>
2015-12-15 20:02:21 +0800

    Finish layout and routes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_title(page_title = '')
+    base_title = "校缘"
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} | {base_title}"
+    end
+  end
 end
