f89f9830fa1cb488003d45df7b6a53f56c34bffe
sealsun <sunkun201309@163.com>
2015-12-16 20:54:26 +0800

    Finish log in/log out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "欢迎来到校缘！让我们缘起校园"
       redirect_to @user
     else
---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,54 @@
+module SessionsHelper
+
+  # 登入指定的用户
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+  
+  # 在持久会话中记住用户
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
+  # 返回当前登录的用户（如果有的话）
+  # update 返回cookie中记忆令牌对应的用户
+  def current_user
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id])
+      #raise  # 测试仍能通过，所以没有覆盖这个分支
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
+    end
+  end
+
+  # 如果用户已登录，返回true，否则返回false
+  def logged_in?
+    !current_user.nil?
+  end
+
+  # 退出当前用户
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
+  end
+
+  # 忘记持久会话
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+
+  # 退出当前用户
+  def log_out 
+    forget(current_user)
+    session.delete(:user_id)
+    @current_user = nil
+  end
+end
